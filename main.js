2e737fa6298597901b56913cdd1a32270ce5a5ca
manigandan0307 <manigandan50300@gmail.com>
2024-08-05 16:58:39 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+/* Get news letter subscibers*/
+function newslettersub(subcribers) {
+  var newsEmail = document.getElementById("newsletter-email").value;
+  alert(
+    "Thank you signing up for our Newsletter. Promotions and Special deals will be sent to " +
+      newsEmail +
+      "!"
+  );
+  
+  }
